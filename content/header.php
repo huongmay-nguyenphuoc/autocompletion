612d762ff0abc3465499b70b9f298fc28518eec7
huongmay-nguyenphuoc <huong-may.nguyen-phuoc@laplateforme.io>
2021-04-29 16:51:12 +0200

    Update header.php

--- content/header.php
+++ content/header.php
@@ -1,7 +1,7 @@
 <div class="navbar-fixed">
     <nav class="teal darken-2">
         <div class="nav-wrapper">
-            <a href="../index.php" class="brand-logo siteTitleLogo left">Le rat de B.</a>
+            <a href="index.php" class="brand-logo siteTitleLogo left">Le rat de B.</a>
             <form id="autocompleteSearch" method="post" class="right">
                 <div class="input-field">
                     <input type="text" id="autocomplete-input" class="autocomplete">
